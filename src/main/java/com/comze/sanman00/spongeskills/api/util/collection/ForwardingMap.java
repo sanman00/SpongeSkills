9926412491c130d38b994d80a87b1fb0c6fd4883
sanman00 <sanjaywygold2011@hotmail.co.uk>
2016-09-22 19:34:36 +0100

    Restructure project to allow Sponge implementation to co-exist with API

--- src/main/java/com/comze/sanman00/spongeskills/api/util/collection/ForwardingMap.java
+++
@@ -1,7 +0,0 @@
-package com.comze.sanman00.spongeskills.api.util.collection;
-
-import java.util.Map;
-
-public interface ForwardingMap<K, V> extends Map<K, V> {
-    Map<K, V> getContainedMap();
-}
