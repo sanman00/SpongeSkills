d20708aba5f6d5208982feaba2ed5f47cb9a5257
sanman00 <sanjaywygold2011@hotmail.co.uk>
2016-09-30 19:58:41 +0100

    Create plugin main class

---
+++ SpongeSkills-sponge/src/main/java/com/comze/sanman00/spongeskills/sponge/Main.java
@@ -0,0 +1,11 @@
+package com.comze.sanman00.spongeskills.sponge;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = Main.PLUGIN_ID, name = Main.PLUGIN_NAME, version = Main.PLUGIN_VERSION, description = Main.PLUGIN_DESC)
+public class Main {
+    public static final String PLUGIN_ID = "spongeskills";
+    public static final String PLUGIN_NAME = "SpongeSkills";
+    public static final String PLUGIN_VERSION = "0.0.0";
+    public static final String PLUGIN_DESC = "A plugin that adds skills to Minecraft";
+}
